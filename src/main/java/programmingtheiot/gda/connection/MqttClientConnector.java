e6c6ab43cff10800b99d1f76cadaedb4b36a3e72
sergioooo0 <sergiogonzalezrodriguez05@gmail.com>
2025-04-10 09:47:49 +0200

    modulo 06-01 completed

--- src/main/java/programmingtheiot/gda/connection/MqttClientConnector.java
+++ src/main/java/programmingtheiot/gda/connection/MqttClientConnector.java
@@ -37,6 +37,20 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 	
 	private static final Logger _Logger =
 		Logger.getLogger(MqttClientConnector.class.getName());
+
+	private boolean useAsyncClient = false;
+	
+	private MqttClient           mqttClient = null;
+	private MqttConnectOptions   connOpts = null;
+	private MemoryPersistence    persistence = null;
+	private IDataMessageListener dataMsgListener = null;
+
+	private String  clientID = null;
+	private String  brokerAddr = null;
+	private String  host = ConfigConst.DEFAULT_HOST;
+	private String  protocol = ConfigConst.DEFAULT_MQTT_PROTOCOL;
+	private int     port = ConfigConst.DEFAULT_MQTT_PORT;
+	private int     brokerKeepAlive = ConfigConst.DEFAULT_KEEP_ALIVE;
 	
 	// params
 	
@@ -50,6 +64,43 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 	public MqttClientConnector()
 	{
 		super();
+		ConfigUtil configUtil = ConfigUtil.getInstance();
+
+		this.host =
+			configUtil.getProperty(
+				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.HOST_KEY, ConfigConst.DEFAULT_HOST);
+
+		this.port =
+			configUtil.getInteger(
+				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.PORT_KEY, ConfigConst.DEFAULT_MQTT_PORT);
+
+		this.brokerKeepAlive =
+			configUtil.getInteger(
+				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.KEEP_ALIVE_KEY, ConfigConst.DEFAULT_KEEP_ALIVE);
+		
+		this.useAsyncClient =
+			configUtil.getBoolean(
+				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.USE_ASYNC_CLIENT_KEY);
+
+		this.clientID = "GDAClient-MQTT-" + java.util.UUID.randomUUID().toString();
+
+
+		// these are specific to the MQTT connection which will be used during connect
+		this.persistence = new MemoryPersistence();
+		this.connOpts = new MqttConnectOptions();
+
+		this.connOpts.setKeepAliveInterval(this.brokerKeepAlive);
+
+		// NOTE: If using a random clientID for each new connection,
+		// clean session should be 'true'; see MQTT spec for details
+		this.connOpts.setCleanSession(false);
+
+		// NOTE: Auto-reconnect can be a useful connection recovery feature
+		this.connOpts.setAutomaticReconnect(true);
+
+		// NOTE: URL does not have a protocol handler for "tcp",
+		// so we need to construct the URL manually
+		this.brokerAddr = this.protocol + "://" + this.host + ":" + this.port;
 	}
 	
 	
@@ -58,12 +109,45 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 	@Override
 	public boolean connectClient()
 	{
+		try {
+			if (this.mqttClient == null) {
+				this.mqttClient = new MqttClient(this.brokerAddr, this.clientID, this.persistence);
+				this.mqttClient.setCallback(this);
+			}
+	
+			if (! this.mqttClient.isConnected()) {
+				_Logger.info("MQTT client connecting to broker: " + this.brokerAddr);
+				this.mqttClient.connect(this.connOpts);
+				return true;
+			} else {
+				_Logger.warning("MQTT client already connected to broker: " + this.brokerAddr);
+			}
+		} catch (MqttException e) {
+			// TODO: handle this exception
+			_Logger.log(Level.SEVERE, "Failed to connect MQTT client to broker.", e);
+		}
+	
 		return false;
 	}
 
 	@Override
 	public boolean disconnectClient()
 	{
+		try {
+			if (this.mqttClient != null) {
+				if (this.mqttClient.isConnected()) {
+					_Logger.info("Disconnecting MQTT client from broker: " + this.brokerAddr);
+					this.mqttClient.disconnect();
+					return true;
+				} else {
+					_Logger.warning("MQTT client not connected to broker: " + this.brokerAddr);
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle this exception
+			_Logger.log(Level.SEVERE, "Failed to disconnect MQTT client from broker: " + this.brokerAddr, e);
+		}
+	
 		return false;
 	}
 
@@ -75,18 +159,21 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 	@Override
 	public boolean publishMessage(ResourceNameEnum topicName, String msg, int qos)
 	{
+		_Logger.info("Publishing message to topic: " + topicName.toString());
 		return false;
 	}
 
 	@Override
 	public boolean subscribeToTopic(ResourceNameEnum topicName, int qos)
 	{
+		_Logger.info("Subscribing to topic: " + topicName.toString());
 		return false;
 	}
 
 	@Override
 	public boolean unsubscribeFromTopic(ResourceNameEnum topicName)
 	{
+		_Logger.info("Unsubscribing from topic: " + topicName.toString());
 		return false;
 	}
 
@@ -99,6 +186,10 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 	@Override
 	public boolean setDataMessageListener(IDataMessageListener listener)
 	{
+		if (listener != null) {
+			this.dataMsgListener = listener;
+			return true;
+		}
 		return false;
 	}
 	
@@ -160,3 +251,11 @@ public class MqttClientConnector implements IPubSubClient, MqttCallbackExtended
 		// TODO: implement this
 	}
 }
+
+/**
+ * This class is part of the Programming the Internet of Things project.
+ * 
+ * It is provided as a simple shell to guide the student and assist with
+ * implementation for the Programming the Internet of Things exercises,
+ * and designed to be modified by the student as needed.
+ */ 
