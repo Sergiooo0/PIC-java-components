02b6023ff8fb8e71a91e603c8d664551c84af0a4
sergioooo0 <sergiogonzalezrodriguez05@gmail.com>
2025-04-15 00:42:52 +0200

    08-03 done

--- src/main/java/programmingtheiot/gda/connection/CoapServerGateway.java
+++ src/main/java/programmingtheiot/gda/connection/CoapServerGateway.java
@@ -23,8 +23,12 @@ import org.eclipse.californium.core.network.Endpoint;
 import org.eclipse.californium.elements.config.UdpConfig;
 
 import programmingtheiot.common.ConfigConst;
- import programmingtheiot.common.IDataMessageListener;
+import programmingtheiot.common.IActuatorDataListener;
+import programmingtheiot.common.IDataMessageListener;
  import programmingtheiot.common.ResourceNameEnum;
+import programmingtheiot.gda.connection.handlers.GetActuatorCommandResourceHandler;
+import programmingtheiot.gda.connection.handlers.UpdateSystemPerformanceResourceHandler;
+import programmingtheiot.gda.connection.handlers.UpdateTelemetryResourceHandler;
 
 /**
  * Shell representation of class for student implementation.
@@ -65,7 +69,11 @@ public class CoapServerGateway
 		
 		this.dataMsgListener = dataMsgListener;
 		
-		initServer();
+		initServer(
+		ResourceNameEnum.CDA_SYSTEM_PERF_MSG_RESOURCE,
+		ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE,
+		ResourceNameEnum.CDA_ACTUATOR_CMD_RESOURCE
+	);
 	}
 
 		
@@ -129,12 +137,87 @@ public class CoapServerGateway
 	
 	// private methods
 	
-	private Resource createResourceChain(ResourceNameEnum resource)
+	private Resource createResourceChain(ResourceNameEnum resourceEnum)
 	{
-		return null;
+		if (resourceEnum == null) return null;
+
+		String fullPath = resourceEnum.getResourceName();
+		String[] pathSegments = fullPath.split("/");
+
+		if (pathSegments.length == 0) return null;
+
+		CoapResource root = new CoapResource(pathSegments[0]);
+		CoapResource current = root;
+
+		for (int i = 1; i < pathSegments.length - 1; i++) {
+			CoapResource child = new CoapResource(pathSegments[i]);
+			current.add(child);
+			current = child;
+		}
+
+		// final segment is the actual leaf resource
+		String leafName = pathSegments[pathSegments.length - 1];
+
+		CoapResource handler = null;
+
+		switch (resourceEnum) {
+			case CDA_SYSTEM_PERF_MSG_RESOURCE:
+				handler = new UpdateSystemPerformanceResourceHandler(leafName);
+				((UpdateSystemPerformanceResourceHandler) handler).setDataMessageListener(this.dataMsgListener);
+				break;
+
+			case CDA_SENSOR_MSG_RESOURCE:
+				handler = new UpdateTelemetryResourceHandler(leafName);
+				((UpdateTelemetryResourceHandler) handler).setDataMessageListener(this.dataMsgListener);
+				break;
+
+			case CDA_ACTUATOR_CMD_RESOURCE:
+				handler = new GetActuatorCommandResourceHandler(leafName);
+				// aquí puedes vincular al DeviceDataManager si fuera necesario
+				break;
+
+			default:
+				_Logger.warning("Unknown resource enum: " + resourceEnum.name());
+				break;
+		}
+
+		if (handler != null) {
+			current.add(handler);
+			return root;
+		} else {
+			return null;
+		}
 	}
+
+
 	
-	private void initServer(ResourceNameEnum ...resources)
+	private void initServer(ResourceNameEnum... resources)
 	{
+		try {
+			this.coapServer = new CoapServer(ConfigConst.DEFAULT_COAP_PORT);
+
+			// Recursos predeterminados si no se pasan como argumentos
+			if (resources == null || resources.length == 0) {
+				resources = new ResourceNameEnum[] {
+					ResourceNameEnum.CDA_SYSTEM_PERF_MSG_RESOURCE,
+					ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE,
+					ResourceNameEnum.CDA_ACTUATOR_CMD_RESOURCE
+				};
+			}
+
+			for (ResourceNameEnum resource : resources) {
+				Resource coapResource = createResourceChain(resource);
+				if (coapResource != null) {
+					this.coapServer.add(coapResource);
+					_Logger.info("Added CoAP resource: " + resource.getResourceName());
+				} else {
+					_Logger.warning("Failed to create CoAP resource: " + resource.getResourceName());
+				}
+			}
+		} catch (Exception e) {
+			_Logger.log(Level.SEVERE, "Exception during CoAP server initialization.", e);
+		}
 	}
+
+
 }
