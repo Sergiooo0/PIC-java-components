7f9fe063e0abc0655bf176c56acb157655607fcf
Sergio <sergiogonzalezrodriguez05@gmail.com>
2025-04-17 00:28:11 +0200

    Test

--- src/test/java/programmingtheiot/part03/integration/app/DeviceDataManagerWithCommsTest.java
+++ src/test/java/programmingtheiot/part03/integration/app/DeviceDataManagerWithCommsTest.java
@@ -16,6 +16,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import programmingtheiot.common.ConfigConst;
+import programmingtheiot.common.ConfigUtil;
 import programmingtheiot.common.ResourceNameEnum;
 import programmingtheiot.data.DataUtil;
 import programmingtheiot.data.SensorData;
@@ -33,16 +35,25 @@ import programmingtheiot.gda.connection.MqttClientConnector;
  */
 public class DeviceDataManagerWithCommsTest
 {
-	// static
-	
-	private static final Logger _Logger =
-		Logger.getLogger(DeviceDataManagerWithCommsTest.class.getName());
-	
-
-	// member var's
-	
+	// static	
+	private DeviceDataManager devDataMgr = null;
 	
 	// test setup methods
+	@Before
+	public void setUp() throws Exception
+	{
+		devDataMgr = new DeviceDataManager();
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		if (devDataMgr != null) {
+			devDataMgr.stopManager();
+		}
+	}
+
+	
 	
 	/**
 	 * @throws java.lang.Exception
@@ -57,22 +68,6 @@ public class DeviceDataManagerWithCommsTest
 	 */
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception
-	{
-	}
-	
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception
-	{
-	}
-	
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception
 	{
 	}
 	
@@ -108,6 +103,62 @@ public class DeviceDataManagerWithCommsTest
 		
 		mqttClient.disconnectClient();
 		devDataMgr.stopManager();
+	}
+
+	@Test
+	public void testSendActuationEventsToCda()
+	{
+		devDataMgr.startManager();
+
+		ConfigUtil cfgUtil = ConfigUtil.getInstance();
+
+		float nominalVal = cfgUtil.getFloat(ConfigConst.GATEWAY_DEVICE, "nominalHumiditySetting");
+		float lowVal     = cfgUtil.getFloat(ConfigConst.GATEWAY_DEVICE, "triggerHumidifierFloor");
+		float highVal    = cfgUtil.getFloat(ConfigConst.GATEWAY_DEVICE, "triggerHumidifierCeiling");
+		int delay        = cfgUtil.getInteger(ConfigConst.GATEWAY_DEVICE, "humidityMaxTimePastThreshold");
+
+		generateAndProcessHumiditySensorDataSequence(devDataMgr, nominalVal, lowVal, highVal, delay);
+	}
+
+	private void generateAndProcessHumiditySensorDataSequence(DeviceDataManager ddm, float nominalVal, float lowVal, float highVal, int delay)
+	{
+		SensorData sd = new SensorData();
+		sd.setName("My Test Humidity Sensor");
+		sd.setLocationID("constraineddevice001");
+		sd.setTypeID(ConfigConst.HUMIDITY_SENSOR_TYPE);
+
+		sd.setValue(nominalVal);
+		ddm.handleSensorMessage(ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, sd);
+		waitForSeconds(2);
+
+		sd.setValue(nominalVal);
+		ddm.handleSensorMessage(ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, sd);
+		waitForSeconds(2);
+
+		sd.setValue(lowVal - 2);
+		ddm.handleSensorMessage(ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, sd);
+		waitForSeconds(delay + 1);
+
+		sd.setValue(lowVal - 1);
+		ddm.handleSensorMessage(ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, sd);
+		waitForSeconds(delay + 1);
+
+		sd.setValue(lowVal + 1);
+		ddm.handleSensorMessage(ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, sd);
+		waitForSeconds(delay + 1);
+
+		sd.setValue(nominalVal);
+		ddm.handleSensorMessage(ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, sd);
+		waitForSeconds(delay + 1);
+	}
+
+	private void waitForSeconds(int seconds)
+	{
+		try {
+			Thread.sleep(seconds * 1000);
+		} catch (InterruptedException e) {
+			// ignore
+		}
 	}
 	
 	/*
